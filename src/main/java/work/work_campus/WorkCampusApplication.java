7030304a61697142f84a836991b7327252a1a7d8
JJadeYoon <yess9904@naver.com>
2024-12-20 01:05:48 +0900

    chore: Initial commit

---
+++ src/main/java/work/work_campus/WorkCampusApplication.java
@@ -0,0 +1,13 @@
+package work.work_campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/work_campus/WorkCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package work.work_campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
