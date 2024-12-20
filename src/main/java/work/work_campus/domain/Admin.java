3fd58f787927a2470c6c114ccd2e63752e4e2a02
JJadeYoon <yess9904@naver.com>
2024-12-21 01:56:13 +0900

    feat: 엔티티 설계

--- src/main/java/work/work_campus/domain/Admin.java
+++ src/main/java/work/work_campus/domain/Admin.java
@@ -1,6 +1,42 @@
 package work.work_campus.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import work.work_campus.global.BaseEntity;
 
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Admin extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String adminLoginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String phone;
+    private String email;
+    private String position;
+
+    @OneToMany(mappedBy = "admin")
+    private List<Department> departments;
 }
--- src/main/java/work/work_campus/domain/Department.java
+++ src/main/java/work/work_campus/domain/Department.java
@@ -1,6 +1,40 @@
 package work.work_campus.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import work.work_campus.global.BaseEntity;
 
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String departmentName;
+
+    private String location;
+
+    @ManyToOne
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+
+    @OneToMany(mappedBy = "department")
+    private List<Student> students;
 }
